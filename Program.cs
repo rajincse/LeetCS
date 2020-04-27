7aafc95077dd580b888bbf364a3d83deba74e7ff
rajinse <rajincse@github.com>
2020-04-26 18:19:16 -0700

    Added IsSameTreeProblem

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LeetCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TreeNode p = new TreeNode(1);
+            p.left = new TreeNode(2);
+            p.right = new TreeNode(1);
+
+            TreeNode q = new TreeNode(1);
+            q.left = new TreeNode(1);
+            q.right = new TreeNode(2);
+
+            Utility.PrettyPrintTree(p);
+            Console.WriteLine($"{p}\n{q}\n IsSame => {new IsSameTreeProblem().IsSameTree(p, q)}");
+        }
+    }
+}
---
+++ TreeNode.cs
@@ -0,0 +1,32 @@
+using System.Text;
+
+public class TreeNode {
+      public int val;
+      public TreeNode left;
+      public TreeNode right;
+      public TreeNode(int x) { val = x; }
+
+      public override string ToString()
+      {
+          return TreeNode.GetTreeString(this);
+      }
+
+      public static string GetTreeString(TreeNode node, string prefix = null, bool isLeft = true) {
+        
+        if (node == null) {            
+            return string.Empty;
+        }
+        StringBuilder sb = new StringBuilder();
+        if(node.right !=null) {
+            sb.Append(GetTreeString(node.right, $"{prefix}{(isLeft ? "│   " : "    ")}", false));
+        }
+
+        sb.AppendLine( $"{prefix }{ (isLeft ? "└── " : "┌── ")}{ node.val}");
+
+        if (node.left !=null) {
+            sb.Append(GetTreeString(node.left, prefix + (isLeft ? "    " : "│   "), true));
+        }
+        return sb.ToString();
+    }
+ }
+ 
