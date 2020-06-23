1b9d81a1eb2fc7f30b68fed9182c7039d6c7ee79
rajinse <rajincse@github.com>
2020-06-22 23:54:51 -0700

    CountingBits

--- Problems/CountingBitsProblem.cs
+++ Problems/CountingBitsProblem.cs
@@ -2,6 +2,40 @@ namespace Problems
 {
     public class CountingBitsProblem
     {
+        public int[] CountBits(int num) {
         
+            if(num < 0)
+            {
+                return  null;
+            }
+            else if(num ==0)
+            {
+                return new int[1]{0};
+            }
+            
+            int[] result = new int[num+1];
+            for(int n=0;n<=num;n++)
+            {
+                result[n] = GetBitCount(n);
+            }
+            
+            return result;
+        }
+        
+        public int GetBitCount(int num)
+        {
+            if(num <= 0)
+            {
+                return 0;
+            }
+            int sum =0;
+            for(int i=0;i<32;i++)
+            {
+                int bit = (num >> (32-i-1)) & 0x01;
+                sum+= bit;
+            }
+            
+            return sum;
+        }
     }
 }
