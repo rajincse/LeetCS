c08a913d01dcf366dfa99875f7bec1ed28e26a60
Sayeed Alam <rajincse@github.com>
2020-09-03 00:35:09 -0700

    AddTwoNumber

---
+++ Problems/AddTwoNumbersProblem.cs
@@ -0,0 +1,40 @@
+using System;
+using Common;
+
+namespace  Problems
+{
+    public class AddTwoNumbersProblem
+    {
+        public ListNode AddTwoNumbers(ListNode l1,
+                                      ListNode l2) {
+            return AddTwoNumbers(l1, l2, 0);
+        }
+        
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2, int carry) {
+            if(l1 == null && l2 == null && carry == 0)
+            {
+                return null;
+            }
+            
+            
+            int sum = (l1 == null? 0: l1.val)
+                    + (l2 == null? 0 : l2.val )
+                    + carry;
+            int digit = sum %10;
+            int newCarry = sum /10;
+            ListNode result = new ListNode(digit);
+            result.next = AddTwoNumbers(l1 == null? null: l1.next,
+                                        l2 == null? null : l2.next , newCarry);
+            
+            return result;
+        }
+
+        public static void Main(string [] args)
+        {
+            ListNode l1 = ListNode.CreateFromInt(new int[]{9,9,9});
+            ListNode l2 = ListNode.CreateFromInt(new int[]{9,8});
+            var result = new AddTwoNumbersProblem().AddTwoNumbers(l1, l2);
+            Console.WriteLine($"{l1} + {l2} => {result}");
+        }
+    }
+}
--- Problems/ReorderDataLogProblem.cs
+++ Problems/ReorderDataLogProblem.cs
@@ -98,14 +98,14 @@ namespace Problems
             var result = list.Select(x=> x.FullValue).ToArray();
             return result;
         }
-        public static void Main(string[] args)
-        {
-            string [] logs = new string[]
-            {
-                "l5sh 6 3869 08 1295", "16o 94884717383724 9", "43 490972281212 3 51", "9 ehyjki ngcoobi mi", "2epy 85881033085988", "7z fqkbxxqfks f y dg", "9h4p 5 791738 954209", "p i hz uubk id s m l", "wd lfqgmu pvklkdp u", "m4jl 225084707500464", "6np2 bqrrqt q vtap h", "e mpgfn bfkylg zewmg", "ttzoz 035658365825 9", "k5pkn 88312912782538", "ry9 8231674347096 00", "w 831 74626 07 353 9", "bxao armngjllmvqwn q", "0uoj 9 8896814034171", "0 81650258784962331", "t3df gjjn nxbrryos b"
-            };
-            var result = new ReorderDataLogProblem().ReorderLogFiles(logs);
-            Console.WriteLine($"{Utility.PrintArray<string>(logs)} => {Utility.PrintArray<string>(result)}");
-        }
+        // public static void Main(string[] args)
+        // {
+        //     string [] logs = new string[]
+        //     {
+        //         "l5sh 6 3869 08 1295", "16o 94884717383724 9", "43 490972281212 3 51", "9 ehyjki ngcoobi mi", "2epy 85881033085988", "7z fqkbxxqfks f y dg", "9h4p 5 791738 954209", "p i hz uubk id s m l", "wd lfqgmu pvklkdp u", "m4jl 225084707500464", "6np2 bqrrqt q vtap h", "e mpgfn bfkylg zewmg", "ttzoz 035658365825 9", "k5pkn 88312912782538", "ry9 8231674347096 00", "w 831 74626 07 353 9", "bxao armngjllmvqwn q", "0uoj 9 8896814034171", "0 81650258784962331", "t3df gjjn nxbrryos b"
+        //     };
+        //     var result = new ReorderDataLogProblem().ReorderLogFiles(logs);
+        //     Console.WriteLine($"{Utility.PrintArray<string>(logs)} => {Utility.PrintArray<string>(result)}");
+        // }
     }
 }
