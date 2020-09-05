679009d530ec4c25bef36a2adbcbd05ea76416b6
Sayeed Alam <rajincse@github.com>
2020-09-04 23:12:42 -0700

    MostCommonWordProblem solved

--- Problems/MostCommonWordProblem.cs
+++ Problems/MostCommonWordProblem.cs
@@ -25,7 +25,7 @@ namespace Problems
             }
         }
         public string MostCommonWord(string paragraph, string[] banned) {
-            if(string.IsNullOrWhiteSpace(paragraph) || banned == null || banned.Length == 0)
+            if(string.IsNullOrWhiteSpace(paragraph) )
             {
                 return null;
             }
@@ -38,17 +38,15 @@ namespace Problems
                 bannedSet.Add(bannedWord.ToLower());
             }
             
-            string[] allWords  = lowerCaseParagraph.Split();
+            string[] allWords  = lowerCaseParagraph.Split(' ', ',', '.', '!', '?', ';', '\'');
             Dictionary<string,WordFrequency> dictionary = new  Dictionary<string,WordFrequency>();
             foreach(string word in allWords)
             {
-                if(!bannedSet.Contains(word))
+                if(!bannedSet.Contains(word) && !string.IsNullOrEmpty(word))
                 {
                     if(dictionary.ContainsKey(word))
                     {
-                        var wordFrequency = dictionary[word];
-                        wordFrequency.Frequency++;
-                        dictionary[word] = wordFrequency;
+                        dictionary[word].Frequency++;
                     }
                     else
                     {
@@ -72,12 +70,12 @@ namespace Problems
             return wordList[0].Word;
         }
 
-        public static void Main(string[] args)
-        {
-            var paragraph = "Bob hit a ball, the hit BALL flew far after it was hit.";
-            var banned = new string[]{"hit"};
-            var result = new MostCommonWordProblem().MostCommonWord(paragraph, banned);
-            Console.WriteLine($"{paragraph}, {Utility.PrintArray<string>(banned)} => {result} ");
-        }
+        // public static void Main(string[] args)
+        // {
+        //     var paragraph = "j. t? T. z! R, v, F' x! L; l! W. M; S. y? r! n; O. q; I? h; w. t; y; X? y, p. k! k, h, J, r? w! U! V; j' u; R! z. s. T' k. P? M' I' j! y. P, T! e; X. w? M! Y, X; G; d, X? S' F, K? V, r' v, v, D, w, K! S? Q! N. n. V. v. t? t' x! u. j; m; n! F, V' Y! h; c! V, v, X' X' t? n; N' r; x. W' P? W; p' q, S' X, J; R. x; z; z! G, U; m. P; o. P! Y; I, I' l' J? h; Q; s? U, q, x. J, T! o. z, N, L; u, w! u, S. Y! V; S? y' E! O; p' X, w. p' M, h! R; t? K? Y' z? T? w; u. q' R, q, T. R? I. R! t, X, s? u; z. u, Y, n' U; m; p? g' P? y' v, o? K? R. Q? I! c, X, x. r' u! m' y. t. W; x! K? B. v; m, k; k' x; Z! U! p. U? Q, t, u' E' n? S' w. y; W, x? r. p! Y? q, Y. t, Z' V, S. q; W. Z, z? x! k, I. n; x? z; V? s! g, U; E' m! Z? y' x? V! t, F. Z? Y' S! z, Y' T? x? v? o! l; d; G' L. L, Z? q. w' r? U! E, H. C, Q! O? w! s? w' D. R, Y? u. w, N. Z? h. M? o, B, g, Z! t! l, W? z, o? z, q! O? u, N; o' o? V; S! z; q! q. o, t! q! w! Z? Z? w, F? O' N' U' p? r' J' L; S. M; g' V. i, P, v, v, f; W? L, y! i' z; L? w. v, s! P?";
+        //     var banned = new string[]{"m","q","e","l","c","i","z","j","g","t","w","v","h","p","d","b","a","r","x","n"};
+        //     var result = new MostCommonWordProblem().MostCommonWord(paragraph, banned);
+        //     Console.WriteLine($"{paragraph}, [{Utility.PrintArray<string>(banned)}] => {result} ");
+        // }
     }
 }
