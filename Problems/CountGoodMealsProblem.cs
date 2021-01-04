f942e8ef586386719a588cdc47e7a3f670bf6748
Sayeed Alam <rajincse@github.com>
2021-01-03 22:36:02 -0800

    Count good meals

--- Problems/CountGoodMealsProblem.cs
+++ Problems/CountGoodMealsProblem.cs
@@ -5,15 +5,14 @@ using Common;
 namespace Problems
 {
     public class CountGoodMealsProblem
-    {        
-        //Unsolved
+    {
         public int CountPairs(int[] deliciousness) {
             if(deliciousness == null || deliciousness.Length == 0)
             {
                 return 0;
             }
 
-            Dictionary<int, int> occurrenceMap = new Dictionary<int, int>();
+            Dictionary<long, long> occurrenceMap = new Dictionary<long, long>();
             for(int i=0;i<deliciousness.Length;i++)
             {
                 if(occurrenceMap.ContainsKey(deliciousness[i]))
@@ -25,15 +24,15 @@ namespace Problems
                     occurrenceMap[deliciousness[i]] = 1;
                 }
             }
-            int totalCount =0;
-            var keyList = new List<int>(occurrenceMap.Keys);
-            keyList.Sort();
-            foreach(var num in keyList)
+            long totalCount =0;
+
+            foreach(var num in occurrenceMap.Keys)
             {
-                int complement = TwosPowerPair( num);
+                if(num ==0) continue;
+                long complement = TwosPowerPair( num);
                 if(IsPowerOfTwo(num) && occurrenceMap.ContainsKey(0))
                 {
-                    totalCount+= occurrenceMap[0];
+                    totalCount+= (occurrenceMap[0]* occurrenceMap[num]);
                 }
                 if(occurrenceMap.ContainsKey(complement))
                 {
@@ -47,20 +46,20 @@ namespace Problems
                     }
                 }
             }
-            return totalCount;
+            return (int)(totalCount % (1_000_000_007));
         }
 
-        private bool IsPowerOfTwo(int num)
+        private bool IsPowerOfTwo(long num)
         {
             return num != 0 && (num & (num-1)) ==0;
         }
 
-        private int TwosPowerPair(int num)
+        private long TwosPowerPair(long num)
         {
-            int twosPower = 1;
+            long twosPower = 1;
             for(int i=0;i< 31;i++)
             {
-                if(num <= twosPower)
+                if(num < twosPower)
                 {
                     break;
                 }
@@ -72,7 +71,13 @@ namespace Problems
 
         // public static void Main(string[] args)
         // {
-        //     var input = new int[]{2160,1936,3,29,27,5,2503,1593,2,0,16,0,3860,28908,6,2,15,49,6246,1946,23,105,7996,196,0,2,55,457,5,3,924,7268,16,48,4,0,12,116,2628,1468};
+        //     var input = new int[100000];
+        //     for(int i=0;i<input.Length;i++)
+        //     {
+        //         input[i] = 32;
+        //     }
+        //     // var input = new int[]{149,107,1,63,0,1,6867,1325,5611,2581,39,89,46,18,12,20,22,234};
+        //     // var input = new int[]{2160,1936,3,29,27,5,2503,1593,2,0,16,0,3860,28908,6,2,15,49,6246,1946,23,105,7996,196,0,2,55,457,5,3,924,7268,16,48,4,0,12,116,2628,1468};
         //     // var input = new int[]{1,1,1,3,3,3,7};
         //     var result = new CountGoodMealsProblem().CountPairs(input);
         //     Console.WriteLine($"{Utility.PrintArray<int>(input)} =>{result}");
