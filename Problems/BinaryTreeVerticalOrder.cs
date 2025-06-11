1ee9061c7b93dcf0962e3089299b9465e322fcde
rajin.cse <rajin.cse@gmail.com>
2025-06-11 15:37:00 -0700

    Added Binary tree vertical

--- Problems/BinaryTreeVerticalOrder.cs
+++ Problems/BinaryTreeVerticalOrder.cs
@@ -1,14 +1,58 @@
 // https://leetcode.com/problems/binary-tree-vertical-order-traversal/?envType=company&envId=facebook&favoriteSlug=facebook-thirty-days
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Common;
 
 namespace Problems
 {
     public class BinaryTreeVerticalOrderProblem
     {
-        public IList<IList<int>> VerticalOrder(TreeNode root) {
-            return null;
+        public class TreeColumn
+        {
+            public TreeNode Node { get; set; }
+            public int ColumnIndex { get; set; }
+
+            public TreeColumn(TreeNode node, int columnIndex)
+            {
+                this.Node = node;
+                this.ColumnIndex = columnIndex;
+            }
+
+        }
+        public IList<IList<int>> VerticalOrder(TreeNode root)
+        {
+            var queue = new Queue<TreeColumn>();
+            var hashTable = new Dictionary<int, List<int>>();
+
+            queue.Enqueue(new TreeColumn(root, 0));
+
+            while (queue.Count != 0)
+            {
+                var treeColumn = queue.Dequeue();
+                if (!hashTable.ContainsKey(treeColumn.ColumnIndex))
+                {
+                    var list = new List<int>();
+                }
+                hashTable[treeColumn.ColumnIndex].Add(treeColumn.Node.val);
+
+                if (treeColumn.Node.left != null)
+                {
+                    queue.Enqueue(new TreeColumn(treeColumn.Node.left, treeColumn.ColumnIndex - 1));
+                }
+
+                if (treeColumn.Node.right != null)
+                {
+                    queue.Enqueue(new TreeColumn(treeColumn.Node.right, treeColumn.ColumnIndex + 1));
+                }
+            }
+            var result = new List<IList<int>>();
+
+            foreach (var kvp in hashTable)
+            {
+                result.Add(kvp.Value.ToList());
+            }
+            return result;
         }
 
         public static void Main(string[] args)
