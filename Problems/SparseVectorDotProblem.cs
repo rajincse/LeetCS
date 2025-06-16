5fa91b9c556735ee8238f79c8033f4f5012e2d3e
rajin.cse <rajin.cse@gmail.com>
2025-06-15 20:29:02 -0700

    Added solution

--- Problems/SparseVectorDotProblem.cs
+++ Problems/SparseVectorDotProblem.cs
@@ -1,21 +1,39 @@
 // https://leetcode.com/problems/dot-product-of-two-sparse-vectors/?envType=company&envId=facebook&favoriteSlug=facebook-thirty-days
 using System;
+using System.Collections.Generic;
 using Common;
 
 namespace Problems
 {
     public class SparseVector
     {
+        public Dictionary<int, int> Map { get; set; }
 
         public SparseVector(int[] nums)
         {
+            this.Map = new Dictionary<int, int>();
 
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != 0)
+                {
+                    this.Map[i] = nums[i];
+                }
+            }
         }
 
         // Return the dotProduct of two sparse vectors
         public int DotProduct(SparseVector vec)
         {
-            return 0;
+            var result = 0;
+            foreach (var i in this.Map.Keys)
+            {
+                if (vec.Map.ContainsKey(i))
+                {
+                    result += this.Map[i] * vec.Map[i];
+                }
+            }
+            return result;
         }
     }
     public class SparseVectorDotProblem
