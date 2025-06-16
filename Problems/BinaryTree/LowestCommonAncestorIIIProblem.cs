61b1916a37c78447fb83df90a5bb6ff31c609533
rajin.cse <rajin.cse@gmail.com>
2025-06-15 21:47:38 -0700

    Added Node data structure

--- Problems/BinaryTree/LowestCommonAncestorIIIProblem.cs
+++ Problems/BinaryTree/LowestCommonAncestorIIIProblem.cs
@@ -1,5 +1,8 @@
 // https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree-iii/?envType=company&envId=facebook&favoriteSlug=facebook-thirty-days
 
+using System;
+using Common;
+
 namespace Problems.BinaryTree
 {
     public class Node
@@ -9,14 +12,36 @@ namespace Problems.BinaryTree
         public Node right;
         public Node parent;
 
+        public TreeNode TreeNode;
+
+        public override string ToString()
+        {
+            return this.TreeNode.ToString();
+        }
+
         public static Node FromTreeNode(Common.TreeNode treeNode)
         {
             if (treeNode is null)
             {
                 return null;
             }
+            var node = new Node();
+            node.TreeNode = treeNode;
+            node.val = treeNode.val;
+            node.left = FromTreeNode(treeNode.left);
+            node.right = FromTreeNode(treeNode.right);
 
+            if (node.left is not null)
+            {
+                node.left.parent = node;
+            }
 
+            if (node.right is not null)
+            {
+                node.right.parent = node;
+            }
+
+            return node;
         }
     }
     public class LowestCommonAncestorIIIProblem
@@ -28,10 +53,10 @@ namespace Problems.BinaryTree
 
         public static void Main(string[] args)
         {
-            var nodeP = Common.TreeNode.StringToTreeNode("[5,6,2,null,null,7,4]");
-            var nodeQ = Common.TreeNode.StringToTreeNode("[1,0,8]");
-            int result = new LowestCommonAncestorIIIProblem().LowestCommonAncestor(nodeP, nodeQ);
-            Console.WriteLine($"Input: \n{node}\n=>{result}");
+            var nodeP = Node.FromTreeNode(Common.TreeNode.StringToTreeNode("[5,6,2,null,null,7,4]"));
+            var nodeQ = Node.FromTreeNode(Common.TreeNode.StringToTreeNode("[1,0,8]"));
+            var result = new LowestCommonAncestorIIIProblem().LowestCommonAncestor(nodeP, nodeQ);
+            Console.WriteLine($"p: \n{nodeP},\nq:\n{nodeQ},\n=>{result}");
         }
     }
 }
