b9d5a203a2fcec7fb3b65dd087c838e30144b89d
rajin.cse <rajin.cse@gmail.com>
2025-06-27 11:19:41 -0700

    Solved

--- Problems/MinRemoveToMakeValidProblem.cs
+++ Problems/MinRemoveToMakeValidProblem.cs
@@ -1,5 +1,7 @@
 // https://leetcode.com/problems/minimum-remove-to-make-valid-parentheses/description/?envType=company&envId=facebook&favoriteSlug=facebook-thirty-days
 using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace Problems
 {
@@ -7,14 +9,53 @@ namespace Problems
     {
         public string MinRemoveToMakeValid(string s)
         {
-            return string.Empty;
+            var stack = new Stack<int>();
+            var chars = s.ToCharArray();
+            var deleteIndices = new bool[chars.Length];
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (chars[i] == ')')
+                {
+                    if (stack.Count > 0)
+                    {
+                        stack.Pop();
+                    }
+                    else
+                    {
+                        deleteIndices[i] = true;    
+                    }
+                    
+                }
+                else if (chars[i] == '(')
+                {
+                    stack.Push(i);
+                }
+            }
+
+            while (stack.Count > 0)
+            {
+                var i = stack.Pop();
+                deleteIndices[i] = true;
+            }
+
+            var result = new StringBuilder();
+            for (int i = 0; i < deleteIndices.Length; i++)
+            {
+                if (!deleteIndices[i])
+                {
+                    result.Append(chars[i]);
+                }
+            }
+
+            return result.ToString();
         }
 
-        public static void Main(string[] args)
-        {
-            var input = "lee(t(c)o)de)";
-            var result = new MinRemoveToMakeValidProblem().MinRemoveToMakeValid(input);
-            Console.WriteLine($"{input} => {result}");
-        }
+        // public static void Main(string[] args)
+        // {
+        //     var input = "a)b(c)d";
+        //     var result = new MinRemoveToMakeValidProblem().MinRemoveToMakeValid(input);
+        //     Console.WriteLine($"{input} => {result}");
+        // }
     }
 }
